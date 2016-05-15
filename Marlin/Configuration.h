36576b1486e7f56fd9d528538732d2879a4f6f4f
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-15 21:00:01 +0300

    Use calibrated X steps for GT2 belt

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,7 +490,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.5, 80.5, 393.8, 160.0}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80.5, 393.8, 160.0}
 #define DEFAULT_MAX_FEEDRATE          {100, 100, 5, 50}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {500,500,90,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
